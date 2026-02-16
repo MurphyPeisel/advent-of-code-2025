7c28975cda281299794bce43372d4e1195e8035d
Murphy Peisel <murphy@pingintel.com>
2026-02-16 17:57:13 -0500

    write part 2. revise main() logic to read input so it can be reused

--- src/day-2/main.cpp
+++ src/day-2/main.cpp
@@ -3,35 +3,37 @@
 #include <sstream>
 #include <string>
 
-/*
-Goal: find invalid IDs. Find repeated sequences of digits.
+std::int64_t partOne(const std::string &input) {
+  /*
+  Goal: find invalid IDs. Find repeated sequences of digits.
 
-Since the young Elf was just doing silly patterns, you can find the invalid IDs
-by looking for any ID which is made only of some sequence of digits repeated
-twice. So, 55 (5 twice), 6464 (64 twice), and 123123 (123 twice) would all be
-invalid IDs.
+  Since the young Elf was just doing silly patterns, you can find the invalid
+  IDs by looking for any ID which is made only of some sequence of digits
+  repeated twice. So, 55 (5 twice), 6464 (64 twice), and 123123 (123 twice)
+  would all be invalid IDs.
 
-None of the numbers have leading zeroes; 0101 isn't an ID at all. (101 is a
-valid ID that you would ignore.)
+  None of the numbers have leading zeroes; 0101 isn't an ID at all. (101 is a
+  valid ID that you would ignore.)
 
-Result: What do you get if you add up all of the invalid IDs?
-*/
+  Result: What do you get if you add up all of the invalid IDs?
+  */
 
-int main() {
   // sum of invalid IDs
   std::int64_t sumOfIDs = 0;
 
   constexpr char RANGE_DELIM = '-';
   constexpr char INPUT_DELIM = ',';
 
-  std::string inputRange = "";
+  std::stringstream ss(input);
+  std::string inputRange;
+
   // loop through input
-  while (std::getline(std::cin, inputRange, INPUT_DELIM)) {
+  while (std::getline(ss, inputRange, INPUT_DELIM)) {
     std::size_t rangeDelimIdx = inputRange.find(RANGE_DELIM);
     std::string leftBoundStr = inputRange.substr(0, rangeDelimIdx);
     std::string rightBoundStr = inputRange.substr(rangeDelimIdx + 1);
-    std::int64_t leftBound = std::stol(leftBoundStr);
-    std::int64_t rightBound = std::stol(rightBoundStr);
+    std::int64_t leftBound = std::stoll(leftBoundStr);
+    std::int64_t rightBound = std::stoll(rightBoundStr);
     // this loop goes through the numbers within the range
     while (leftBound <= rightBound) {
       const std::string currNum = std::to_string(leftBound);
@@ -45,6 +47,77 @@ int main() {
       ++leftBound;
     }
   }
-  std::cout << "Final sum of invalid IDs: " << sumOfIDs << std::endl;
+  return sumOfIDs;
+}
+
+std::int64_t partTwo(const std::string &input) {
+  /*
+  Goal: find invalid IDs. Find repeated sequences of digits.
+
+  Now, an ID is invalid if it is made only of some sequence of digits repeated
+  at least twice. So, 12341234 (1234 two times), 123123123 (123 three times),
+  1212121212 (12 five times), and 1111111 (1 seven times) are all invalid IDs.
+
+  Result: What do you get if you add up all of the invalid IDs?
+  */
+
+  // sum of invalid IDs
+  std::int64_t sumOfIDs = 0;
+
+  constexpr char RANGE_DELIM = '-';
+  constexpr char INPUT_DELIM = ',';
+
+  std::stringstream ss(input);
+  std::string inputRange;
+
+  // loop through input
+  while (std::getline(ss, inputRange, INPUT_DELIM)) {
+    // construct range bounds
+    std::size_t rangeDelimIdx = inputRange.find(RANGE_DELIM);
+    std::string leftBoundStr = inputRange.substr(0, rangeDelimIdx);
+    std::string rightBoundStr = inputRange.substr(rangeDelimIdx + 1);
+    std::int64_t leftBound = std::stoll(leftBoundStr);
+    std::int64_t rightBound = std::stoll(rightBoundStr);
+    // this loop goes through the numbers within the range
+    while (leftBound <= rightBound) {
+      const std::string currNum = std::to_string(leftBound);
+      const std::size_t len = currNum.size();
+      // check num split into i subdivisions
+      for (std::size_t i = 2; i <= len; ++i) {
+        // check if number subdivision is valid
+        if (len % i != 0) {
+          continue;
+        }
+        int sequenceSize = len / i;
+        int numEqual = 0;
+        // check if all subdivisions are equal
+        for (std::size_t  j = 0; j < i; ++j) {
+          if (currNum.substr(sequenceSize * j, sequenceSize) ==
+              currNum.substr(0, sequenceSize)) {
+            ++numEqual;
+          } else {
+            break;
+          }
+        }
+        if (numEqual == i) {
+          sumOfIDs += leftBound;
+          break;
+        }
+      }
+      ++leftBound;
+    }
+  }
+  return sumOfIDs;
+}
+
+int main() {
+  std::string fullInput;
+  std::getline(std::cin, fullInput);
+
+  std::int64_t partOneSolution = partOne(fullInput);
+  std::cout << "Part 1 -- sum of invalid IDs: " << partOneSolution << std::endl;
+  std::int64_t partTwoSolution = partTwo(fullInput);
+  std::cout << "Part 2 -- sum of invalid IDs: " << partTwoSolution << std::endl;
+
   return 0;
 }
