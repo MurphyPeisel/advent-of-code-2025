e3b8a44aa432e9ccab8c9c02d49ca69b013a103c
Murphy Peisel <murphy@pingintel.com>
2026-03-08 19:05:01 -0400

    working solution for day 3 part 2

--- src/day-3/main.cpp
+++ src/day-3/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <math.h>
 
 std::int64_t partOne(const std::vector<std::string>& allBanks) {
     int64_t totalJolts = 0;
@@ -31,6 +32,36 @@ std::int64_t partOne(const std::vector<std::string>& allBanks) {
     return totalJolts;
 }
 
+std::int64_t partTwo(const std::vector<std::string>& allBanks) {
+    int64_t totalJolts = 0;
+
+    // find the idx of the largest number with at least 11 numbers following it
+    // find the idx of the next largest number with at least 10 numbers following it
+    // find the idx of the next largest number with at least 9 numbers following it
+    // ...
+
+    for (const std::string& bankJolts : allBanks) {
+        int bankLen = bankJolts.size();
+        int batteriesRemaining = 12;
+        std::size_t leftIdx = 0;
+        while (batteriesRemaining > 0) {
+            std::size_t probeIdx = leftIdx + 1;
+            int largestNum = 0;
+            while ((bankLen - probeIdx >= batteriesRemaining)) {
+                if ((bankJolts[leftIdx] < bankJolts[probeIdx])) {
+                    leftIdx = probeIdx;
+                }
+                ++probeIdx;
+            }
+            largestNum = bankJolts[leftIdx] - '0';
+            totalJolts += largestNum * pow(10, batteriesRemaining-1);
+            --batteriesRemaining;
+            ++leftIdx;
+        }
+    }
+    return totalJolts;
+}
+
 int main() {
     std::vector<std::string> allBanks;
     std::string bank;
@@ -38,7 +69,11 @@ int main() {
         allBanks.push_back(bank);
     }
 
-    std::int64_t totalJolts = partOne(allBanks);
-    std::cout << "Total output joltage: " << totalJolts << std::endl;
+    // std::int64_t totalJoltsPartOne = partOne(allBanks);
+    // std::cout << "Part one total output joltage: " << totalJoltsPartOne << std::endl;
+
+    std::int64_t totalJoltsPartTwo = partTwo(allBanks);
+    std::cout << "Part two total output joltage: " << totalJoltsPartTwo << std::endl;
+
     return 0;
 }
